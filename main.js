822304cf0d779decb3fd638e71e461544e3229b9
kobebest47 <65650614+kobebest47@users.noreply.github.com>
2020-05-30 06:47:12 +0800

    new commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var name=prompt ("你好，你叫捨麼名字");
+alert("Hi "+ name +" 準備好開始測驗嗎")
+
+function randomValue(){
+  var n=Math.random();
+      n=n*100;
+      n=Math.floor(n)+1;
+      alert(n + "%");
+  document.querySelector('output').value;
+ }
